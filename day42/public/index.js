371cf21f1015aaed9cc8ec7d7e8a19852a48e044
hwong0305 <hwong.0305@gmail.com>
2021-04-20 23:52:44 -0700

    Add watch function

--- day42/public/index.js
+++ day42/public/index.js
@@ -1,13 +1,17 @@
+import Fuse from 'fuse.js';
 const cardBody = document.querySelector('.card-body');
+const search = document.getElementById('search');
+
+let people;
+let fuse;
 
 fetch('https://randomuser.me/api?results=50')
   .then(r => r.json())
   .then(data => {
-    const { results } = data;
+    people = data.results;
+    fuse = new Fuse(people, options);
 
-    console.log(results);
-
-    results.forEach(result => {
+    people.forEach(result => {
       const div = document.createElement('div');
       div.innerHTML = `<img src="${result.picture.thumbnail}" /><div><h1>${result.name.first} ${result.name.last}</h1><p>${result.location.city}, ${result.location.country}</p></div>`;
       div.className = 'member';
@@ -15,3 +19,32 @@ fetch('https://randomuser.me/api?results=50')
       cardBody.appendChild(div);
     });
   });
+
+const options = {
+  threshold: 0.3,
+  keys: ['name.first', 'name.last'],
+};
+
+search.addEventListener('keydown', e => {
+  console.log('changing');
+  cardBody.innerHTML = '';
+  if (!e.target.value) {
+    people.forEach(result => {
+      const div = document.createElement('div');
+      div.innerHTML = `<img src="${result.picture.thumbnail}" /><div><h1>${result.name.first} ${result.name.last}</h1><p>${result.location.city}, ${result.location.country}</p></div>`;
+      div.className = 'member';
+
+      cardBody.appendChild(div);
+    });
+  } else {
+    const results = fuse.search(e.target.value);
+
+    results.forEach(({ item: result }) => {
+      const div = document.createElement('div');
+      div.innerHTML = `<img src="${result.picture.thumbnail}" /><div><h1>${result.name.first} ${result.name.last}</h1><p>${result.location.city}, ${result.location.country}</p></div>`;
+      div.className = 'member';
+
+      cardBody.appendChild(div);
+    });
+  }
+});
