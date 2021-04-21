f16854c9e5c61fb4d99772e26d57b8829f4f61b5
hwong0305 <hwong.0305@gmail.com>
2021-04-20 22:56:33 -0700

    working on layout

--- day42/public/index.js
+++ day42/public/index.js
@@ -1,5 +1,16 @@
+const cardBody = document.querySelector('.card-body');
+
 fetch('https://randomuser.me/api?results=50')
   .then(r => r.json())
   .then(data => {
-    console.log(data);
+    const { results } = data;
+
+    console.log(results);
+
+    results.forEach(result => {
+      const div = document.createElement('div');
+      div.innerHTML = `<h1>${result.name.first} ${result.name.last}</h1><p>${result.location.city}, ${result.location.country}</p>`;
+
+      cardBody.appendChild(div);
+    });
   });
